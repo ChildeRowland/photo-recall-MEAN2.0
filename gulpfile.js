44fc00869b87925da9f203949e2f2f3a207a71e4
Christopher Mehmed <chrismehmed@gmail.com>
2016-07-08 19:33:14 -0400

    npm script build:heroku

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var del = require('del');
+
+var deployment = 'heroku';
+
+gulp.task('clean', function() {
+	del([ (deployment + '/**/*'), '!.git']);
+});
+
+gulp.task('build', function() {
+	return gulp.src('dist/**/*')
+		.pipe(gulp.dest(deployment));
+});
