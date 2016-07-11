aca93d3f6d1c99f2ac99f6d4281bf99aee5be31d
Christopher Mehmed <chrismehmed@gmail.com>
2016-07-10 22:56:37 -0400

    config external db

--- src/server/db.js
+++ src/server/db.js
@@ -1,10 +1,12 @@
+var config = require('./config');
+
 var env = process.env.NODE_ENV || 'development';
 var db;
 
 if ( env === 'production' ) {
-	db = 'mongolab';
+	db = config.production.database.db;
 } else {
-	db = 'localhost:27017/cmex-deletable';
+	db = config.development.database.db;
 }
 
 module.exports = db;
